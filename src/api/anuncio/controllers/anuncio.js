e2c78dc5127c170c4a2e02ff2eaf6bdbde3ce137
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2024-04-29 18:13:13 -0700

    removed template type collections, add Anuncio collection

--- src/api/anuncio/controllers/anuncio.js
+++ src/api/anuncio/controllers/anuncio.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * anuncio controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::anuncio.anuncio');
